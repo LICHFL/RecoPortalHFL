881070f8d3eeaf45809f0cb91701c3d0ec6086d6
70766 <70766@LICHFLDC1994.HFLHO.com>
2023-09-24 11:05:06 +0530

    added column validation for users

--- RecoPortal/src/main/java/com/lichfl/entity/BrsUserDetails.java
+++ RecoPortal/src/main/java/com/lichfl/entity/BrsUserDetails.java
@@ -6,32 +6,35 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Table(name = "VW_BRS_USER_DETAILS")
 public class BrsUserDetails {
 
 	@Id
-	@Column(name="USRLOGIN")
+	@Column(name = "USRLOGIN", nullable = false)
 	private String usrLogin;
-	
-	@Column(name="USRBRANCHCODE")
+
+	@Column(name = "USRBRANCHCODE")
 	private String usrBranchCode;
-	
-	@Column(name="BANK_CODE")
+
+	@Column(name = "BANK_CODE")
 	private String bankcode;
-	
-	@Column(name="USREMAIL")
+
+	@Column(name = "USREMAIL")
 	private String usrEmail;
-	
-	@Column(name="USRDESG")
+
+	@Column(name = "USRDESG")
 	private String usrDesg;
-	
-	@Column(name = "USERNAME")
+
+	@Column(name = "USERNAME", nullable = false)
 	private String userName;
 
 }
